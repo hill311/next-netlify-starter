d893a59f4db6e1e5006b392c8615d1b636c44657
hill311 <77602181+hill311@users.noreply.github.com>
2022-11-01 22:48:12 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,7 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
-}
+<iframe
+    src="https://gateway.ipfscdn.io/ipfs/QmST2eo8EGsn1TsFp1oC5kffo3SqWQeNtikr9jiWMwfrTf/nft-drop.html?contract=0xC7CD683B01065851fA7Cb96aD0f92eF10E328737&chainId=5"
+    width="600px"
+    height="600px"
+    style="max-width:100%;"
+    frameborder="0"
+    ></iframe>
